cb7dadd644105dfa39bf6abce1232dd01f1ed637
shuzaei <shuzaei@gmail.com>
2021-05-16 03:14:55 +0900

    initial commit

---
+++ csdb.sh
@@ -0,0 +1,25 @@
+if [ $# -ne 1 ]
+then
+    echo "Usage: csdb filename"
+    exit 1
+else
+    echo "csdb: \e[34mcompile\e[0m $1.cs"
+    message=`mcs -debug "$1.cs" -out:"$1" 2>&1`
+    result=$?
+
+    if [ $result -ne 0 ]
+    then
+        echo ""
+        IFS=$'\n'
+        for line in $message
+        do
+            echo "csdb: mcs: $line"
+        done
+        echo ""
+        echo "csdb: \e[31mcompile failed\e[0m"
+    else
+        echo "csdb: \e[32mcompile success\e[0m"
+        echo "csdb: \e[34mdebug\e[0m $1"
+        mono --debug $1
+    fi
+fi

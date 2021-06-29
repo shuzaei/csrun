7dddd88080d32f4405e45d582502ff5e9ae49147
Shuzaei <shuzaei@gmail.com>
2021-06-29 15:25:10 +0900

    db->run

--- csrun.sh
+++ csrun.sh
@@ -3,7 +3,7 @@ then
     printf "Usage: csrun <file>\n"
     exit 1
 else
-    printf "csdb: \033[34mcompile\033[0m $1.cs\n"
+    printf "csrun: \033[34mcompile\033[0m $1.cs\n"
     message=`mcs "$1.cs" -out:"$1" 2>&1`
     result=$?
 
@@ -13,13 +13,13 @@ else
         IFS=$'\n'
         for line in $message
         do
-            printf "csdb: mcs: $line\n"
+            printf "csrun: mcs: $line\n"
         done
         printf "\n"
-        printf "csdb: \033[31mcompile failed\033[0m\n"
+        printf "csrun: \033[31mcompile failed\033[0m\n"
     else
-        printf "csdb: \033[32mcompile success\033[0m\n"
-        printf "csdb: \033[34mrun\033[0m $1\n"
+        printf "csrun: \033[32mcompile success\033[0m\n"
+        printf "csrun: \033[34mrun\033[0m $1\n"
         mono $1
     fi
 fi
